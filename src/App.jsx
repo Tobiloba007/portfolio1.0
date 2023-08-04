6051c9f8c814d9e00a3c87932b8ee8a30b712c2f
Michael <Olaniyanmichael0@gmail.com>
2023-08-04 23:32:27 +0100

    build error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Intro from "./components/Intro"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 import Reviews from "./components/Reviews"
-import Socials from "./components/socials"
+import Socials from "./components/Socials"
 
 const App = () => {
   return (
